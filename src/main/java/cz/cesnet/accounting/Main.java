9d03635c5b21f8912f29d93d83593318882f1a2f
Martin Kuba <makub@ics.muni.cz>
2023-06-15 17:05:01 +0200

    replaced RowMappers with DataClassRowMapper

--- src/main/java/cz/cesnet/accounting/Main.java
+++ src/main/java/cz/cesnet/accounting/Main.java
@@ -10,9 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.DataClassRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
-import org.springframework.stereotype.Component;
 import org.springframework.util.function.ThrowingConsumer;
 
 import java.io.File;
@@ -22,12 +21,11 @@ import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
-import java.util.Date;
 
 @SpringBootApplication
-@Component
 public class Main implements CommandLineRunner {
 
     private static final Logger log = LoggerFactory.getLogger(Main.class);
@@ -45,7 +43,7 @@ public class Main implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         log.info("starting");
-        Timestamp time = new Timestamp(ZonedDateTime.now().minusDays(3).toInstant().toEpochMilli());
+        Timestamp time = new Timestamp(ZonedDateTime.parse("2023-06-01T12:00:00+02:00").toInstant().toEpochMilli());
 
         log.info("rows as list of Maps");
         List<Map<String, Object>> rows = jdbc.queryForList("SELECT * FROM workload WHERE day>=? LIMIT 5", time);
@@ -57,7 +55,10 @@ public class Main implements CommandLineRunner {
         }
 
         log.info("rows as List of Workload instances");
-        List<Workload> workloads = jdbc.query("SELECT id,day,cpu,jobs_time,utilization_ratio FROM workload WHERE day>=? LIMIT 5", WORKLOAD_ROW_MAPPER, time);
+        List<Workload> workloads = jdbc.query(
+                "SELECT id,day,cpu,jobs_time,utilization_ratio FROM workload WHERE day>=? LIMIT 5",
+                new DataClassRowMapper<>(Workload.class),
+                time);
         // write rows into a CSV file
         try (SequenceWriter sw = csvSequenceWriter(Workload.class, "workloads.csv")) {
             for (Workload workload : workloads) {
@@ -67,7 +68,21 @@ public class Main implements CommandLineRunner {
 
         log.info("rows as stream of Job instances");
         try (SequenceWriter sw = csvSequenceWriter(Job.class, "jobs.csv")) {
-            jdbc.queryForStream("SELECT acct_id_string, jobname, queue, start_time, end_time FROM acct_pbs_record LIMIT 2000000", JOB_ROW_MAPPER)
+            jdbc.queryForStream("""
+                                    SELECT
+                                        acct_id_string,
+                                        jobname,
+                                        queue,
+                                        to_timestamp(start_time) as start_time,
+                                        to_timestamp(end_time) as end_time
+                                    FROM acct_pbs_record
+                                    WHERE start_time>?
+                                    ORDER BY start_time
+                                    LIMIT 20
+                                    """,
+                            new DataClassRowMapper<>(Job.class),
+                            time.toInstant().getEpochSecond() // linux time (seconds since epoch)
+                    )
                     .forEach(ThrowingConsumer.of(sw::write));
         }
     }
@@ -75,30 +90,11 @@ public class Main implements CommandLineRunner {
     record Workload(int id, LocalDate day, int cpu, long jobsTime, float utilizationRatio) {
     }
 
-    final static RowMapper<Workload> WORKLOAD_ROW_MAPPER = (rs, rowNum) -> new Workload(
-            rs.getInt("id"),
-            rs.getObject("day", LocalDate.class),
-            rs.getInt("cpu"),
-            rs.getLong("jobs_time"),
-            rs.getFloat("utilization_ratio")
-    );
-
     /**
      * Record for keeping job data.
      */
-    record Job(String id, String jobname, String queue, Date start_time, Date end_time) {
+    record Job(String acct_id_string, String jobname, String queue, Date start_time, Date end_time) {
     }
-
-    /**
-     * Lambda that maps ResultSet rows into Job instances.
-     */
-    final static RowMapper<Job> JOB_ROW_MAPPER = (rs, rowNum) -> new Job(
-            rs.getString("acct_id_string"),
-            rs.getString("jobname"),
-            rs.getString("queue"),
-            new Date(rs.getLong("start_time") * 1000L),
-            new Date(rs.getLong("end_time") * 1000L)
-    );
 
     /**
      * Creates a SequenceWriter for writing rows into a CSV file with columns named after the provided class.
